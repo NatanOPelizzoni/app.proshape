1f6295ee2b24c7216220b7341b9b6282c96ec33b
Natan O Pelizzoni <natan2624@gmail.com>
2023-11-24 00:47:20 -0300

    refactor: removed all material imports

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -3,12 +3,6 @@ import { CommonModule } from '@angular/common';
 import { RegisterComponent } from './components/register/register.component';
 import { PublicRoutingModule } from './public-routing.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCardModule } from '@angular/material/card';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatDividerModule } from '@angular/material/divider';
 import { LoginComponent } from './components/login/login.component';
 import { MaterialModule } from 'src/app/shared/material/material.module';
 
